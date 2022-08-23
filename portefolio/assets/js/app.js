122e775f6a432af6329e105c36973e52ab461a48
devcut <guillaume.egloff@gmail.com>
2022-08-23 16:09:12 +0200

    Add preview text and modal content project

--- portefolio/assets/js/app.js
+++ portefolio/assets/js/app.js
@@ -1,5 +1,19 @@
 import '../scss/app.scss';
+import 'jquery';
 import InteractiveBoard from 'interactive-board';
+import 'jquery-modal';
+
+$('a.open-modal').click(function() {
+    $(this).modal({
+        fadeDuration: 250
+    })
+    return false;
+});
+
+// Hack for replace the modal after the action on close
+$('.modal').on($.modal.AFTER_CLOSE, function(event, modal) {
+    $(this).insertAfter($(modal.$anchor));
+});
 
 let ib = new InteractiveBoard({
     activateRandomShape: {
